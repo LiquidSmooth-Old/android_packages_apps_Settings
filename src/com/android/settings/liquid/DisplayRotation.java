fb765f62b9e6cd562f90edba037089f695398590
Laszlo David <laszlo.david@gmail.com>
2013-02-16 01:23:12 +0100

    Swap volume buttons when the screen is rotated (2/2)
    See: http://review.cyanogenmod.org/#/c/14530/
    
    Patch Set 8: Invert the orientation interpretations on tablets
    
    Change-Id: I201febc2827e6fbd347cb0e3f8d3347ad1bc487b
    
    Conflicts:
    
    	media/java/android/media/AudioManager.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	media/java/android/media/AudioManager.java

--- src/com/android/settings/liquid/DisplayRotation.java
+++ src/com/android/settings/liquid/DisplayRotation.java
@@ -28,6 +28,7 @@ import android.provider.Settings;
 
 import com.android.internal.view.RotationPolicy;
 import com.android.settings.R;
+import com.android.internal.util.liquid.DeviceUtils;
 import com.android.settings.SettingsPreferenceFragment;
 
 public class DisplayRotation extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
@@ -38,12 +39,14 @@ public class DisplayRotation extends SettingsPreferenceFragment implements OnPre
     private static final String ROTATION_90_PREF = "display_rotation_90";
     private static final String ROTATION_180_PREF = "display_rotation_180";
     private static final String ROTATION_270_PREF = "display_rotation_270";
+    private static final String KEY_SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
 
     private CheckBoxPreference mAccelerometer;
     private CheckBoxPreference mRotation0Pref;
     private CheckBoxPreference mRotation90Pref;
     private CheckBoxPreference mRotation180Pref;
     private CheckBoxPreference mRotation270Pref;
+    private CheckBoxPreference mSwapVolumeButtons;
 
     public static final int ROTATION_0_MODE = 1;
     public static final int ROTATION_90_MODE = 2;
@@ -81,6 +84,13 @@ public class DisplayRotation extends SettingsPreferenceFragment implements OnPre
         mRotation90Pref.setChecked((mode & ROTATION_90_MODE) != 0);
         mRotation180Pref.setChecked((mode & ROTATION_180_MODE) != 0);
         mRotation270Pref.setChecked((mode & ROTATION_270_MODE) != 0);
+
+        mSwapVolumeButtons = (CheckBoxPreference) prefSet.findPreference(KEY_SWAP_VOLUME_BUTTONS);
+        if (mSwapVolumeButtons != null) {
+            int swapVolumeKeys = Settings.System.getInt(getContentResolver(),
+                    Settings.System.SWAP_VOLUME_KEYS_ON_ROTATION, 0);
+            mSwapVolumeButtons.setChecked(swapVolumeKeys > 0);
+        }
     }
 
     @Override
@@ -136,6 +146,11 @@ public class DisplayRotation extends SettingsPreferenceFragment implements OnPre
             Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
                     Settings.System.ACCELEROMETER_ROTATION_ANGLES, mode);
             return true;
+        } else if (preference == mSwapVolumeButtons) {
+            int value = mSwapVolumeButtons.isChecked() ?
+                    (DeviceUtils.isTablet(getActivity()) ? 2 : 1) : 0;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.SWAP_VOLUME_KEYS_ON_ROTATION, value);
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
