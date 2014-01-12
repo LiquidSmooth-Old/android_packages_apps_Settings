bcf8fe716eb53eb29875487405abba5e8611c883
ILWT <tsubus@lagat.org>
2014-01-12 13:50:22 +0100

    Add Fast Charge QS Tile [2/2]
    Change-Id: Iafd5ee0eefa9f7abd41b03ea7fa4e976c573e61c
    
    Conflicts:
    	res/values/custom_strings.xml
    	src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
    
    Conflicts:
    	src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java
    
    Conflicts:
    	src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java

--- src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java
@@ -55,6 +55,7 @@ import static com.android.internal.util.liquid.QSConstants.TILE_VOLUME;
 import static com.android.internal.util.liquid.QSConstants.TILE_WIFI;
 import static com.android.internal.util.liquid.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.liquid.QSConstants.TILE_REBOOT;
+import static com.android.internal.util.liquid.QSConstants.TILE_FCHARGE;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -170,6 +171,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_CONTACT, R.string.title_tile_contact,
                 "com.android.systemui:drawable/ic_qs_default_user"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_FCHARGE, R.string.title_tile_fcharge,
+                "com.android.systemui:drawable/ic_qs_fcharge_off"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
@@ -222,6 +226,10 @@ public class QuickSettingsUtil {
             removeTile(TILE_TORCH);
         }
 
+        // Don't show the Fast charge tile if not supported by kernel
+        if (!DeviceUtils.fchargeEnabled(context)) {
+            removeTile(TILE_FCHARGE);
+        }
     }
 
     public static ArrayList<String> getAllDynamicTiles(Context context) {
