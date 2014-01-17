b001f2808d40f1c8e685abb30de643d1c73a91a7
Jorrit Jongma <git@jongma.org>
2014-01-17 10:41:41 +0100

    Launch SuperSU from Settings
    If SuperSU is installed, show an entry in Settings below
    "Developer options", which launches SuperSU when tapped.
    At boot time SuperSU will detect if com.android.settings
    contains the resources from this patch, and if so it
    will hide itself from the launcher. This was implemented
    in v1.85, so we only show the entry if the installed
    SuperSU version is at least that.
    
    Conflicts:
    	res/xml/dashboard_categories.xml
    	src/com/android/settings/SettingsActivity.java
    
    Change-Id: Ib5ed28203a3a1e3e1f54689c9a8a3b74e0917ed1

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1202,11 +1202,14 @@ public class SettingsActivity extends Activity
                             UserManager.DISALLOW_DEBUGGING_FEATURES)) {
                         removeTile = true;
                     }
-                } else if (id == R.id.superuser) {
-                    String value = SystemProperties.get(
-                            DevelopmentSettings.ROOT_ACCESS_PROPERTY, "0");
-                    if (Integer.valueOf(value) == 0 || um.hasUserRestriction(
-                            UserManager.DISALLOW_DEBUGGING_FEATURES)) {
+                } else if (id == R.id.supersu_settings) {
+                    // Embedding into Settings is supported from SuperSU v1.85 and up
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("eu.chainfire.supersu", 0).versionCode >= 185);
+                    } catch (PackageManager.NameNotFoundException e) {
+                    }
+                    if (!supported) {
                         removeTile = true;
                     }
                 } else if (id == R.id.bitsyko_layers) {
@@ -1379,4 +1382,3 @@ public class SettingsActivity extends Activity
         mResultIntentData = resultIntentData;
     }
 }
-
