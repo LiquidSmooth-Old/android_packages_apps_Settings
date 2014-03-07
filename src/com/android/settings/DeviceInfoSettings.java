573ddf6b150eaa8f515578040f237db9c636025d
sspencer10 <sspencer10@gmail.com>
2014-03-06 21:15:40 -0600

    Give the OTA app a new home
    Conflicts:
    	res/values/liquid_strings.xml
    	res/xml/device_info_settings.xml
    	src/com/android/settings/DeviceInfoSettings.java

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -70,6 +70,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
     private static final String KEY_DEVICE_CPU = "device_cpu";
     private static final String KEY_DEVICE_MEMORY = "device_memory";
     private static final String KEY_MOD_BUILD_DATE = "build_date";
+    private static final String KEY_OTA_APP = "ota_app";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
