40d69e88bc52d20164708011ae26a6b8d1a20c48
UtkarshGupta <utkarsh.eminem@gmail.com>
2014-03-08 12:31:41 +0530

    [2/2] OmniGears: Lockscreen blur enhancements
    Adds an option to set blur intensity
    
    Base: https://gerrit.omnirom.org/#/c/5705/
    
    Change-Id: Ife0941c00ebf1cbfc5804a059d337cf4e8528666

--- src/com/android/settings/liquid/LockscreenStyle.java
+++ src/com/android/settings/liquid/LockscreenStyle.java
@@ -36,6 +36,7 @@ import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceGroup;
 import android.preference.PreferenceScreen;
+import android.preference.SeekBarPreference;
 import android.provider.MediaStore;
 import android.provider.Settings;
 import android.util.Log;
@@ -78,6 +79,7 @@ public class LockscreenStyle extends SettingsPreferenceFragment
 
 
     private static final String KEY_SEE_THROUGH = "see_through";
+    private static final String KEY_BLUR_RADIUS = "blur_radius";
 
     private String mDefault;
 
@@ -86,6 +88,7 @@ public class LockscreenStyle extends SettingsPreferenceFragment
     private ColorPickerPreference mLockColor;
     private ColorPickerPreference mDotsColor;
     private CheckBoxPreference mSeeThrough;
+    private SeekBarPreference mBlurRadius;
     private ColorPickerPreference mTargetsColor;
     private ColorPickerPreference mMiscColor;
 
@@ -180,12 +183,19 @@ public class LockscreenStyle extends SettingsPreferenceFragment
                 R.string.lockscreen_misc_color_summary), miscColor);
         mMiscColor.setNewPreviewColor(miscColor);
 
-        // lockscreen see through
+        // lock screen see through
         mSeeThrough = (CheckBoxPreference) findPreference(KEY_SEE_THROUGH);
         if (mSeeThrough != null) {
             mSeeThrough.setChecked(Settings.System.getInt(getContentResolver(),
                     Settings.System.LOCKSCREEN_SEE_THROUGH, 0) == 1);
         }
+
+        // lock screen blur radius
+        mBlurRadius = (SeekBarPreference) findPreference(KEY_BLUR_RADIUS);
+        mBlurRadius.setProgress(Settings.System.getInt(getContentResolver(),
+                Settings.System.LOCKSCREEN_BLUR_RADIUS, 12));
+        mBlurRadius.setOnPreferenceChangeListener(this);
+        mBlurRadius.setEnabled(mSeeThrough.isChecked() && mSeeThrough.isEnabled());
 
         // No lock-slider is available
         boolean dotsDisabled = new LockPatternUtils(getActivity()).isSecure()
@@ -297,6 +307,11 @@ public class LockscreenStyle extends SettingsPreferenceFragment
             setPreferenceSummary(preference,
                     getResources().getString(R.string.lockscreen_dots_color_summary), val);
             return true;
+        } else if (preference == mBlurRadius) {
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.LOCKSCREEN_BLUR_RADIUS,
+                    (Integer) newValue);
+            return true;
         } else if (preference == mTargetsColor) {
             int val = Integer.valueOf(String.valueOf(newValue));
             Settings.Secure.putInt(getContentResolver(),
@@ -321,6 +336,7 @@ public class LockscreenStyle extends SettingsPreferenceFragment
             Settings.System.putInt(getContentResolver(),
                     Settings.System.LOCKSCREEN_SEE_THROUGH,
                     mSeeThrough.isChecked() ? 1 : 0);
+            mBlurRadius.setEnabled(mSeeThrough.isChecked() && mSeeThrough.isEnabled());
             return true;
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
