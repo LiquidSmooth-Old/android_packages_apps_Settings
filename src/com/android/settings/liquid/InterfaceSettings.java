7be011c63e7503d47f97d5b59abbe0aca3170e74
liquid0624 <rjbirnesser@gmail.com>
2014-05-01 13:31:02 -0400

    Remove hardware buttons when device has none

--- src/com/android/settings/liquid/InterfaceSettings.java
+++ src/com/android/settings/liquid/InterfaceSettings.java
@@ -53,6 +53,7 @@ public class InterfaceSettings extends SettingsPreferenceFragment
 
     private static final String KEY_LCD_DENSITY = "lcd_density";
     private static final String DENSITY_PROP = "persist.sys.lcd_density";
+    private static final String KEY_HARDWARE_KEYS = "hardwarekeys_settings";
 
     private static final int DIALOG_CUSTOM_DENSITY = 101;
 
@@ -83,6 +84,13 @@ public class InterfaceSettings extends SettingsPreferenceFragment
         }
         mLcdDensity.setSummary(getResources().getString(R.string.current_lcd_density) + current);
         mLcdDensity.setOnPreferenceChangeListener(this);
+
+        PreferenceScreen hardwareKeys = (PreferenceScreen) findPreference(KEY_HARDWARE_KEYS);
+        int deviceKeys = getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+        if (deviceKeys == 0 && hardwareKeys != null) {
+            getPreferenceScreen().removePreference(hardwareKeys);
+        }
     }
 
     @Override
