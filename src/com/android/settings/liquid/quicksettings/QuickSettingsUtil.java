3be8f6a9a587bd511a3fcea62c44245045c76ad9
Schubi <schubi@erlangen.ccc.de>
2014-06-17 11:15:35 +0200

    HOVER: Add QS Tile [2/2]
    Change-Id: Ide431e64c8bbbb87737469aaaf399fd12c94f135
    
    Conflicts:
    	res/values/liquid_strings.xml
    	src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java

--- src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java
+++ src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java
@@ -62,6 +62,7 @@ import static com.android.internal.util.liquid.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.liquid.QSConstants.TILE_FCHARGE;
 import static com.android.internal.util.liquid.QSConstants.TILE_ONTHEGO;
 import static com.android.internal.util.liquid.QSConstants.TILE_PROFILE;
+import static com.android.internal.util.liquid.QSConstants.TILE_HOVER;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -198,6 +199,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_BATTERYSAVER, R.string.title_tile_batterysaver,
                 "com.android.systemui:drawable/ic_qs_battery_saver_on"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_HOVER, R.string.title_tile_hover,
+                "com.android.systemui:drawable/ic_qs_hover_on"));
     }
 
     private static void registerTile(QuickSettingsUtil.TileInfo info) {
