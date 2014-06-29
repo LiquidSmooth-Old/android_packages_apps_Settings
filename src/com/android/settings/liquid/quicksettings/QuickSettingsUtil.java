TheKang/packages_apps_Settings

QuickSettings: Added ProfileTile [2/2]
see frameworks commit for details

Patchset 2:
  using blunden's png resource
  making ProfileSettings reachable from the outside

Patchset 3:
  The profile tile should not be available if profiles are turned off

Side rant: I really hate manifest cleanups. i would wish for our
settings not beeing such a mess and comply with fragments properly.

Change-Id: I3e7bcce4b5f54b9bed065780779c9946a73936be

src/com/android/settings/slim/quicksettings/QuickSettingsUtil.java
@@ -57,6 +57,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_WIFI;
 import static com.android.internal.util.slim.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.slim.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.slim.QSConstants.TILE_FCHARGE;
+import static com.android.internal.util.slim.QSConstants.TILE_PROFILE;
 
 import android.content.ContentResolver;
 import android.content.Context;
@@ -176,6 +177,9 @@ public class QuickSettingsUtil {
                 TILE_FCHARGE, R.string.title_tile_fcharge,
                 "com.android.systemui:drawable/ic_qs_fcharge_on"));
         registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_PROFILE, R.string.title_tile_profile,
+                "com.android.systemui:drawable/ic_qs_profiles"));
+        registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_SHAKE, R.string.title_tile_shake,
                 "com.android.systemui:drawable/ic_qs_shake_events"));
     }
@@ -234,7 +238,6 @@ public class QuickSettingsUtil {
         if (!DeviceUtils.deviceSupportsFastCharge(context)) {
             removeTile(TILE_FCHARGE);
         }
-
 
     }
 

LiquidSmooth-Old/android_packages_apps_Settings

Conflicts:
	res/values/liquid_strings.xml
	src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java

src/com/android/settings/liquid/quicksettings/QuickSettingsUtil.java
@@ -60,6 +60,7 @@ import static com.android.internal.util.liquid.QSConstants.TILE_WIFIAP;
 import static com.android.internal.util.liquid.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.liquid.QSConstants.TILE_FCHARGE;
 import static com.android.internal.util.liquid.QSConstants.TILE_ONTHEGO;
+import static com.android.internal.util.liquid.QSConstants.TILE_PROFILE;
 import static com.android.internal.util.liquid.QSConstants.TILE_HOVER;
 
 import android.content.ContentResolver;
@@ -191,6 +192,9 @@ public class QuickSettingsUtil {
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_BATTERYSAVER, R.string.title_tile_batterysaver,
                 "com.android.systemui:drawable/ic_qs_battery_saver_on"));
+        registerTile(new QuickSettingsUtil.TileInfo(
+                TILE_PROFILE, R.string.title_tile_profile,
+                "com.android.systemui:drawable/ic_qs_profiles"));
         registerTile(new QuickSettingsUtil.TileInfo(
                 TILE_HOVER, R.string.title_tile_hover,
                 "com.android.systemui:drawable/ic_qs_hover_on"));