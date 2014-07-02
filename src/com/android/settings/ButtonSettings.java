38896fbc7bc76c686c85fdc34b9ca7757e503d69
Danny Baumann <dannybaumann@web.de>
2014-07-02 13:19:56 +0200

    Fix exception being thrown if CMHW is not installed.
    Happens during debugging when mmp'ing the app.
    
    Change-Id: Ief84a812696ef3f2515701790f96649cbde0f7d9

--- src/com/android/settings/ButtonSettings.java
+++ src/com/android/settings/ButtonSettings.java
@@ -164,7 +164,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
         // Only visible on devices that does not have a navigation bar already,
         // and don't even try unless the existing keys can be disabled
         boolean needsNavigationBar = false;
-        if (KeyDisabler.isSupported()) {
+        if (isKeyDisablerSupported()) {
             try {
                 IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
                 needsNavigationBar = wm.needsNavigationBar();
@@ -427,7 +427,7 @@ public class ButtonSettings extends SettingsPreferenceFragment implements
     }
 
     public static void restoreKeyDisabler(Context context) {
-        if (!KeyDisabler.isSupported()) {
+        if (!isKeyDisablerSupported()) {
             return;
         }
 
