Z-ROM/packages_apps_Settings

Add Weather Status Bar Header (4/4)
PS3: Add new weather menu
     Alert dialog is WIP
     icon is WIP
PS4: fix build
PS5: AlertDialog show inverse value
PS5: DialogAlert reboot working
PS6: Added icon
PS7: Fix whitespace
PS8: Fix toast message
PS9: Refix notificationmessage
PS10: Add missing \n

Change-Id: Id4a1afc899156299aa97fb41d283f67107b32cc7

Conflicts:
	res/values/strings_aicp.xml
	res/xml/aicp_lab_prefs.xml

Settings: Option to hide weather icon in statusbar (2/3)

Change-Id: Ia549abdd6f066a63a895e9957ebbe042cc09f50f

Conflicts:
	res/values/strings_aicp.xml

src/com/android/settings/ose/Weather.java
@@ -1,0 +1,180 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.ose;
+
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.os.ServiceManager;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.Gravity;
+
+import java.util.ArrayList;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import com.android.settings.ose.util.Helpers;
+
+public class Weather extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener, OnPreferenceClickListener {
+    private static final String TAG = "WeatherSettings";
+
+    private static final String WEATHER_START = "weather_start";
+    // Package name of the weather app
+    public static final String WEATHER_PACKAGE_NAME = "org.codefirex.cfxweather";
+    // Intent for launching the weather main actvity
+    public static Intent INTENT_WEATHER = new Intent(Intent.ACTION_MAIN)
+            .setClassName(WEATHER_PACKAGE_NAME, WEATHER_PACKAGE_NAME + ".WeatherActivity");
+
+    private static final String PREF_SYSTEMUI_WEATHER_HEADER_VIEW = "cfx_systemui_header_weather_view";
+    private static final String PREF_SYSTEMUI_WEATHER_NOTIFICATION = "cfx_weather_notification";
+    private static final String PREF_SYSTEMUI_WEATHER_ICON = "cfx_weather_icon";
+
+    private CheckBoxPreference mWeatherHeader;
+    private CheckBoxPreference mWeatherNotification;
+    private CheckBoxPreference mWeatherIcon;
+
+    private Preference mWeather;
+
+    private Context Context;
+    private PowerManager pm;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        addPreferencesFromResource(R.xml.ose_weather);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mWeather = (Preference)
+                prefSet.findPreference(WEATHER_START);
+
+        boolean enableWeatherHeader = Settings.System.getBoolean(getContentResolver(),
+                Settings.System.SYSTEMUI_WEATHER_HEADER_VIEW, false);
+        mWeatherHeader = (CheckBoxPreference) prefSet.findPreference(PREF_SYSTEMUI_WEATHER_HEADER_VIEW);
+        mWeatherHeader.setChecked(enableWeatherHeader);
+        mWeatherHeader.setOnPreferenceChangeListener(this);
+
+        boolean enableWeatherNotification = Settings.System.getBoolean(getContentResolver(),
+                Settings.System.SYSTEMUI_WEATHER_NOTIFICATION, false);
+        mWeatherNotification = (CheckBoxPreference) prefSet.findPreference(PREF_SYSTEMUI_WEATHER_NOTIFICATION);
+        mWeatherNotification.setChecked(enableWeatherNotification);
+        mWeatherNotification.setOnPreferenceChangeListener(this);
+
+        boolean enableWeatherIcon = Settings.System.getBoolean(getContentResolver(),
+                Settings.System.SYSTEMUI_WEATHER_ICON, false);
+        mWeatherIcon = (CheckBoxPreference) prefSet.findPreference(PREF_SYSTEMUI_WEATHER_ICON);
+        mWeatherIcon.setChecked(enableWeatherIcon);
+        mWeatherIcon.setOnPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        boolean value;
+        if (preference == mWeather) {
+            startActivity(INTENT_WEATHER);
+        } else {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        final String key = preference.getKey();
+        if (preference == mWeatherHeader) {
+            Settings.System.putBoolean(resolver,
+                    Settings.System.SYSTEMUI_WEATHER_HEADER_VIEW,
+                    ((Boolean) objValue) ? true : false);
+            Helpers.restartSystemUI();
+        } else if (preference == mWeatherNotification) {
+            Settings.System.putBoolean(resolver,
+                    Settings.System.SYSTEMUI_WEATHER_NOTIFICATION,
+                    ((Boolean) objValue) ? true : false);
+            openWeatherWarning();
+        } else if (preference == mWeatherIcon) {
+            Settings.System.putBoolean(resolver,
+                    Settings.System.SYSTEMUI_WEATHER_ICON,
+                    ((Boolean) objValue) ? true : false);
+            openWeatherWarning();
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        return false;
+    }
+
+    private void openWeatherWarning() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                .setTitle(getResources().getString(R.string.weather_alert_title))
+                .setMessage(getResources().getString(R.string.weather_alert_message))
+                .setIconAttribute(android.R.attr.alertDialogIcon)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            Handler handle = new Handler();
+                            // Allow statusbar to collapse if desired
+                            handle.postDelayed(new Runnable() {
+                                public void run() {
+                                    PowerManager pm =
+                                            (PowerManager) Weather.this.getSystemService(
+                                            Context.POWER_SERVICE);
+                                    pm.reboot("");
+                                    Weather.this.finish();
+                               }
+                            }, 500);
+                        }
+                }).setNegativeButton(android.R.string.cancel, null);
+        builder.show();
+    }
+}

LiquidSmooth-Old/android_packages_apps_Settings

	res/xml/misc_tweaks.xml

src/com/android/settings/liquid/Weather.java
@@ -1,0 +1,180 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.liquid;
+
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PowerManager;
+import android.os.ServiceManager;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.Gravity;
+
+import java.util.ArrayList;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+import com.android.settings.liquid.util.Helpers;
+
+public class Weather extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener, OnPreferenceClickListener {
+    private static final String TAG = "WeatherSettings";
+
+    private static final String WEATHER_START = "weather_start";
+    // Package name of the weather app
+    public static final String WEATHER_PACKAGE_NAME = "org.codefirex.cfxweather";
+    // Intent for launching the weather main actvity
+    public static Intent INTENT_WEATHER = new Intent(Intent.ACTION_MAIN)
+            .setClassName(WEATHER_PACKAGE_NAME, WEATHER_PACKAGE_NAME + ".WeatherActivity");
+
+    private static final String PREF_SYSTEMUI_WEATHER_HEADER_VIEW = "cfx_systemui_header_weather_view";
+    private static final String PREF_SYSTEMUI_WEATHER_NOTIFICATION = "cfx_weather_notification";
+    private static final String PREF_SYSTEMUI_WEATHER_ICON = "cfx_weather_icon";
+
+    private CheckBoxPreference mWeatherHeader;
+    private CheckBoxPreference mWeatherNotification;
+    private CheckBoxPreference mWeatherIcon;
+
+    private Preference mWeather;
+
+    private Context Context;
+    private PowerManager pm;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        addPreferencesFromResource(R.xml.liquid_weather);
+
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mWeather = (Preference)
+                prefSet.findPreference(WEATHER_START);
+
+        boolean enableWeatherHeader = Settings.System.getBoolean(getContentResolver(),
+                Settings.System.SYSTEMUI_WEATHER_HEADER_VIEW, false);
+        mWeatherHeader = (CheckBoxPreference) prefSet.findPreference(PREF_SYSTEMUI_WEATHER_HEADER_VIEW);
+        mWeatherHeader.setChecked(enableWeatherHeader);
+        mWeatherHeader.setOnPreferenceChangeListener(this);
+
+        boolean enableWeatherNotification = Settings.System.getBoolean(getContentResolver(),
+                Settings.System.SYSTEMUI_WEATHER_NOTIFICATION, false);
+        mWeatherNotification = (CheckBoxPreference) prefSet.findPreference(PREF_SYSTEMUI_WEATHER_NOTIFICATION);
+        mWeatherNotification.setChecked(enableWeatherNotification);
+        mWeatherNotification.setOnPreferenceChangeListener(this);
+
+        boolean enableWeatherIcon = Settings.System.getBoolean(getContentResolver(),
+                Settings.System.SYSTEMUI_WEATHER_ICON, false);
+        mWeatherIcon = (CheckBoxPreference) prefSet.findPreference(PREF_SYSTEMUI_WEATHER_ICON);
+        mWeatherIcon.setChecked(enableWeatherIcon);
+        mWeatherIcon.setOnPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        boolean value;
+        if (preference == mWeather) {
+            startActivity(INTENT_WEATHER);
+        } else {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        final String key = preference.getKey();
+        if (preference == mWeatherHeader) {
+            Settings.System.putBoolean(resolver,
+                    Settings.System.SYSTEMUI_WEATHER_HEADER_VIEW,
+                    ((Boolean) objValue) ? true : false);
+            Helpers.restartSystemUI();
+        } else if (preference == mWeatherNotification) {
+            Settings.System.putBoolean(resolver,
+                    Settings.System.SYSTEMUI_WEATHER_NOTIFICATION,
+                    ((Boolean) objValue) ? true : false);
+            openWeatherWarning();
+        } else if (preference == mWeatherIcon) {
+            Settings.System.putBoolean(resolver,
+                    Settings.System.SYSTEMUI_WEATHER_ICON,
+                    ((Boolean) objValue) ? true : false);
+            openWeatherWarning();
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        return false;
+    }
+
+    private void openWeatherWarning() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity())
+                .setTitle(getResources().getString(R.string.weather_alert_title))
+                .setMessage(getResources().getString(R.string.weather_alert_message))
+                .setIconAttribute(android.R.attr.alertDialogIcon)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            Handler handle = new Handler();
+                            // Allow statusbar to collapse if desired
+                            handle.postDelayed(new Runnable() {
+                                public void run() {
+                                    PowerManager pm =
+                                            (PowerManager) Weather.this.getSystemService(
+                                            Context.POWER_SERVICE);
+                                    pm.reboot("");
+                                    Weather.this.finish();
+                               }
+                            }, 500);
+                        }
+                }).setNegativeButton(android.R.string.cancel, null);
+        builder.show();
+    }
+}