c5c26044d37d9685d74bd7794c807131d3a7f79b
deadman96385 <seanhoyt963@gmail.com>
2014-08-19 13:30:12 +0500

    Add LiquidSmooth Statusbar Settings
    Included just network traffic meter for now
    Credit to mahdi-rom, Haus1, htchoi1995 for the network traffic code
    
    Change-Id: I70b737552981f35b551c2b506f1544d0be431edb

---
+++ src/com/android/settings/liquid/StatusBarSettings.java
@@ -0,0 +1,35 @@
+package com.android.settings.liquid;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class StatusBarSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.liquid_statusbar_settings);
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        return false;
+    }
+
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        boolean value;
+ 		return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
+}
