SlimRoms/packages_apps_Settings

Add slim version to about screen
Change-Id: I4d54b8dd02c3fb75eac1a5f4fffce59611555192

src/com/android/settings/DeviceInfoSettings.java
@@ -63,6 +63,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
     private static final String KEY_UPDATE_SETTING = "additional_system_update_settings";
     private static final String KEY_EQUIPMENT_ID = "fcc_equipment_id";
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
+    private static final String KEY_SLIM_VERSION = "slim_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -93,6 +94,7 @@ public class DeviceInfoSettings extends RestrictedSettingsFragment {
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
+        setValueSummary(KEY_SLIM_VERSION, "ro.slim.version");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);

LiquidSmooth-Old/android_packages_apps_Settings

Conflicts:
	res/values/slim_strings.xml
	res/xml/device_info_settings.xml
	src/com/android/settings/DeviceInfoSettings.java

src/com/android/settings/DeviceInfoSettings.java
@@ -79,6 +79,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_EQUIPMENT_ID = "ro.ril.fccid";
     private static final String KEY_DEVICE_FEEDBACK = "device_feedback";
     private static final String KEY_SAFETY_LEGAL = "safetylegal";
+    private static final String KEY_LIQUID_VERSION = "liquid_version";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -101,6 +102,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
+        setValueSummary(KEY_LIQUID_VERSION, "ro.liquid.version");
 
         if (!SELinux.isSELinuxEnabled()) {
             String status = getResources().getString(R.string.selinux_status_disabled);