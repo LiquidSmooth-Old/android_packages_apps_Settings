0745fd002b23a0fdefb18167e20e37aa43d4b693
Darescu Ionut <ionut.darescu@gmail.com>
2014-11-13 04:17:02 -0500

    Fix derp

--- src/com/android/settings/liquid/SystemCheckBoxPreference.java
+++ src/com/android/settings/liquid/SystemCheckBoxPreference.java
@@ -58,7 +58,6 @@ public class SystemCheckBoxPreference extends CheckBoxPreference {
                 getKey(), defaultReturnValue ? 1 : 0) != 0;
     }
 
-    @Override
     protected boolean isPersisted() {
         // Using getString instead of getInt so we can simply check for null
         // instead of catching an exception. (All values are stored as strings.)
