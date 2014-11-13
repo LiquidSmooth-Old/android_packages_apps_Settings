a43ac5114e659a8194716d5f9fd4cf230f4ef1d4
Robbie Lancaster <robbielancaster811@gmail.com>
2014-11-12 17:14:27 -0800

    Blacklist (2/8)
    Add back blacklist settings.
    
    Conflicts:
    	AndroidManifest.xml
    	res/values/cm_arrays.xml
    	res/values/cm_strings.xml
    
    Add missing blacklist xxhdpi drawables.
    
    Settings: Alert user when they attempt to add an invalid number to BL.
    
      - BlacklistProvider will reject numbers that aren't valid, silently. So instead of
      resuming to a blank screen, we should alert the user that the number they attempted to
      add is invalid by utilizing BlacklistUtils#addOrUpdate method.
    
    Conflicts:
    	res/values/cm_strings.xml
    
    Settings : Launch only contacts picker for blacklist entry
    
    Blacklist: Fix launching blacklist settings
    
    Settings: materialize Blacklist settings
    
    * Add a FAB
    * Add a (generic system) enable/disable switch bar
    
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	src/com/android/settings/cyanogenmod/BaseSystemSettingSwitchBar.java
    	src/com/android/settings/profiles/ProfileEnabler.java
    	src/com/android/settings/profiles/ProfilesList.java
    	src/com/android/settings/profiles/ProfilesSettings.java
    	src/com/android/settings/slim/BaseSystemSettingSwitchBar.java
    
    Conflicts:
    	res/values/slim_colors.xml
    	res/values/styles.xml
    
    Settings: add a way to open blacklist settings externally
    
    Opening the activity doesn't theme the switch bar - an alias works.
    
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    blacklist: make it possible to unblacklist phone/messages via checkboxes
    
    Settings: fix blacklist settings crash
    
    On a device rotate, the MultiSelectListPreference would try and restore
    its state since the preferences aren't persisted. Clear the persisted
    flag so the MultiSelectPreference doesn't try and restore the states, as
    they get populated in onResume().
    
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Fix up some blacklist things
    
    Signed-off-by: RobbieL811 <RobbieLancaster811@gmail.com>
    
    Fix blacklist setting FC
    
    Conflicts:
    	AndroidManifest.xml
    	res/values/slim_arrays.xml
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java
    
    Change-Id: I9f13595c2ccf395f40e5c356c1c26590dd182c35

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -73,6 +73,7 @@ import com.android.settings.applications.ExpandedDesktopPreferenceFragment;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
@@ -310,7 +311,8 @@ public class SettingsActivity extends Activity
             QuickLaunchSettings.class.getName(),
             ApnSettings.class.getName(),
             MainSettings.class.getName(),
-            ExpandedDesktopPreferenceFragment.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            BlacklistSettings.class.getName()
     };
 
 
