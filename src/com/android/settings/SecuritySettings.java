d39b89940c6cf0afbe468bdb53d119ce3f8192ca
Lars Greiss <kufikugel@googlemail.com>
2014-11-17 15:01:30 +0100

    Settings: some fixes and addaptions to LP for PGM
    - add a reset icon - backup icon has changed which we used in the past
    - fix appops details call
    - reorder PGM position.
    
    Change-Id: I8de6da483651bc83a5f711a07633e1c3393a1611

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -201,12 +201,17 @@ public class SecuritySettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.security_settings);
         root = getPreferenceScreen();
 
+        // Add options for device encryption
+        mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
+
+        if (mIsPrimary) {
+            // App security settings
+            addPreferencesFromResource(R.xml.security_settings_app_slim);
+        }
+
         // Add options for lock/unlock screen
         final int resid = getResIdForLockUnlockScreen(getActivity(), mLockPatternUtils);
         addPreferencesFromResource(resid);
-
-        // Add options for device encryption
-        mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
 
         if (!mIsPrimary) {
             // Rename owner info settings
@@ -345,11 +350,6 @@ public class SecuritySettings extends SettingsPreferenceFragment
         if (um.hasUserRestriction(UserManager.DISALLOW_INSTALL_UNKNOWN_SOURCES)
                 || um.hasUserRestriction(UserManager.DISALLOW_INSTALL_APPS)) {
             mToggleAppInstallation.setEnabled(false);
-        }
-
-        if (mIsPrimary) {
-            // App security settings
-            addPreferencesFromResource(R.xml.security_settings_app_slim);
         }
 
         // Advanced Security features
