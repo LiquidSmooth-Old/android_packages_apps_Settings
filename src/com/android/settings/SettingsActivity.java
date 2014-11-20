82c4703fcbb1fb0fef2c8dcb81fc0d395b872a87
Darescu Ionut <ionut.darescu@gmail.com>
2014-11-20 17:22:10 -0500

    Intergrate Viper4Android into settings
    Change-Id: I5c97efb4c2ea786d0ca6d8a6a6b4cb2586e471a6

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1210,8 +1210,18 @@ public class SettingsActivity extends Activity
                     if (!hasDeviceKeys) {
                         removeTile = true;
                     }
-                }
+                } else if (id == R.id.equalizer_settings) {
+                // Embedding into Settings only if app exists (user could manually remove it)
+                boolean supported = false;
+                try {
+                    supported = (getPackageManager().getPackageInfo("com.vipercn.viper4android_v2", 0).versionCode >= 18);
+                } catch (PackageManager.NameNotFoundException e) {
 
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                    }
+                }
                 if (UserHandle.MU_ENABLED && UserHandle.myUserId() != 0
                         && !ArrayUtils.contains(SETTINGS_FOR_RESTRICTED, id)) {
                     removeTile = true;
