b5071a6b8add4919895006bedace366bf876a5f2
Danesh M <daneshm90@gmail.com>
2014-12-01 09:29:57 -0600

    Settings: Add proximity wake option
    Ports and combines:
    
    http://review.cyanogenmod.org/#/c/66658/
    Settings : Add preference for proximity wake
    
    http://review.cyanogenmod.org/#/c/69347/
    Proximity Wake-Up : adjust strings
    
    http://review.cyanogenmod.org/#/c/69803/
    Settings : Make proximity opt-in
    
    Change-Id: Iea0853eccf764438c7aaac76afd13c364fcea0ec

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -89,6 +89,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_WAKEUP_CATEGORY = "category_wakeup_options";
     private static final String KEY_VOLUME_WAKE = "pref_volume_wake";
     private static final String KEY_TAP_TO_WAKE = "double_tap_wake_gesture";
+    private static final String KEY_PROXIMITY_WAKE = "proximity_on_wake";
 
     private static final String CATEGORY_ADVANCED = "advanced_display_prefs";
 
@@ -240,6 +241,13 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         if (!isTapToWakeSupported()) {
             advancedPrefs.removePreference(mTapToWake);
             mTapToWake = null;
+        }
+
+        boolean proximityCheckOnWait = getResources().getBoolean(
+                com.android.internal.R.bool.config_proximityCheckOnWake);
+        if (!proximityCheckOnWait) {
+            advancedPrefs.removePreference(findPreference(KEY_PROXIMITY_WAKE));
+            Settings.System.putInt(getContentResolver(), Settings.System.PROXIMITY_ON_WAKE, 1);
         }
     }
 
