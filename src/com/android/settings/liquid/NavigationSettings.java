fc0b888b2c05a34e298727833b93597ba389f9f9
deadman96385 <seanhoyt963@gmail.com>
2014-12-18 00:36:47 -0500

    Move navbar height to liquid nav
    Change-Id: I8cf4b270dad910d8a495c1fc9c79e88d6a34af49

--- src/com/android/settings/liquid/NavigationSettings.java
+++ src/com/android/settings/liquid/NavigationSettings.java
@@ -32,17 +32,35 @@ import com.android.settings.Utils;
 
 public class NavigationSettings extends SettingsPreferenceFragment {
 
+    private static final String KEY_NAVIGATION_BAR_HEIGHT = "navigation_bar_height";
+
+    private ListPreference mNavigationBarHeight;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.liquid_navigation_settings);
+
+        mNavigationBarHeight = (ListPreference) findPreference(KEY_NAVIGATION_BAR_HEIGHT);
+        mNavigationBarHeight.setOnPreferenceChangeListener(this);
+        int statusNavigationBarHeight = Settings.System.getInt(getActivity().getApplicationContext()
+                .getContentResolver(),
+                Settings.System.NAVIGATION_BAR_HEIGHT, 48);
+        mNavigationBarHeight.setValue(String.valueOf(statusNavigationBarHeight));
+        mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntry());
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
 
-        return false;
+        if (preference == mNavigationBarHeight) {
+            int statusNavigationBarHeight = Integer.valueOf((String) objValue);
+            int index = mNavigationBarHeight.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.NAVIGATION_BAR_HEIGHT, statusNavigationBarHeight);
+            mNavigationBarHeight.setSummary(mNavigationBarHeight.getEntries()[index]);
+        }
+        return true;
     }
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
