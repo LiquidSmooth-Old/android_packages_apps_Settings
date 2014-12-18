a6e535453a53792a1131b92f8f4b42c4a382c2ef
deadman96385 <seanhoyt963@gmail.com>
2014-12-17 23:38:49 -0500

    Move notification settings out of sounds
    Credit: @timpohladthomas and most of the work
    
    Change-Id: Ida812d434950fc0d1a29201b80f99f178dac566e

--- src/com/android/settings/liquid/LiquidnotificationSettings.java
+++ src/com/android/settings/liquid/LiquidnotificationSettings.java
@@ -16,29 +16,161 @@
 
 package com.android.settings.liquid;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceCategory;
+import android.preference.SwitchPreference;
+import android.preference.TwoStatePreference;
+import android.provider.SearchIndexableResource;
 import android.preference.PreferenceScreen;
 import android.provider.Settings;
 import android.provider.Settings.SettingNotFoundException;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 
+import com.android.settings.notification.*;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.Utils;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 public class LiquidnotificationSettings extends SettingsPreferenceFragment {
 
+    private static final String KEY_NOTIFICATION = "notification";
+    private static final String KEY_NOTIFICATION_PULSE = "notification_pulse";
+    private static final String KEY_LOCK_SCREEN_NOTIFICATIONS = "lock_screen_notifications";
+    private static final String KEY_NOTIFICATION_ACCESS = "manage_notification_access";
+    private static final String KEY_NOTIFICATION_LIGHT = "notification_light";
+    private static final String KEY_BATTERY_LIGHT = "battery_light";
+    private static final String PREF_HEADS_UP_SNOOZE_TIME = "heads_up_snooze_time";
+    private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
+
+    private final SettingsObserver mSettingsObserver = new SettingsObserver();
+    private final Handler mHandler = new Handler();
+
+    private Context mContext;
+    private PackageManager mPM;
+
+    private TwoStatePreference mNotificationPulse;
+    private DropDownPreference mLockscreen;
+    private Preference mNotificationAccess;
+    private boolean mSecure;
+    private int mLockscreenSelectedValue;
+
+    private ListPreference mHeadsUpSnoozeTime;
+    private ListPreference mHeadsUpTimeOut;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mContext = getActivity();
+        mPM = mContext.getPackageManager();
+        mSecure = new LockPatternUtils(getActivity()).isSecure();
 
         addPreferencesFromResource(R.xml.liquid_notifications_settings);
+
+        final PreferenceCategory notification = (PreferenceCategory)		
+	                findPreference(KEY_NOTIFICATION);		
+	        initPulse(notification);		
+	        initLockscreenNotifications(notification);		
+
+	        mNotificationAccess = findPreference(KEY_NOTIFICATION_ACCESS);		
+	        refreshNotificationListeners();		
+	
+	        Resources systemUiResources;		
+	        try {		
+	            systemUiResources =		
+	                    getPackageManager().getResourcesForApplication("com.android.systemui");		
+	        } catch (Exception e) {		
+	            return;		
+	        }		
+			
+	        mHeadsUpSnoozeTime = (ListPreference) findPreference(PREF_HEADS_UP_SNOOZE_TIME);		
+	        mHeadsUpSnoozeTime.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {		
+	            @Override		
+	            public boolean onPreferenceChange(Preference preference, Object newValue) {		
+	                int headsUpSnoozeTime = Integer.valueOf((String) newValue);		
+	                updateHeadsUpSnoozeTimeSummary(headsUpSnoozeTime);		
+	                return Settings.System.putInt(getContentResolver(),		
+	                        Settings.System.HEADS_UP_SNOOZE_TIME,		
+	                        headsUpSnoozeTime);		
+	            }		
+	        });		
+	        final int defaultSnoozeTime = systemUiResources.getInteger(systemUiResources.getIdentifier(		
+                        "com.android.systemui:integer/heads_up_snooze_time", null, null));		
+            final int headsUpSnoozeTime = Settings.System.getInt(getContentResolver(),		
+                    Settings.System.HEADS_UP_SNOOZE_TIME, defaultSnoozeTime);		
+    	        mHeadsUpSnoozeTime.setValue(String.valueOf(headsUpSnoozeTime));		
+            updateHeadsUpSnoozeTimeSummary(headsUpSnoozeTime);		
+
+            mHeadsUpTimeOut = (ListPreference) findPreference(PREF_HEADS_UP_TIME_OUT);		
+            mHeadsUpTimeOut.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {		
+                @Override		
+                public boolean onPreferenceChange(Preference preference, Object newValue) {		
+                    int headsUpTimeOut = Integer.valueOf((String) newValue);		
+                    updateHeadsUpTimeOutSummary(headsUpTimeOut);		
+                    return Settings.System.putInt(getContentResolver(),		
+                            Settings.System.HEADS_UP_NOTIFCATION_DECAY,		
+                            headsUpTimeOut);		
+                }		
+            });		
+            final int defaultTimeOut = systemUiResources.getInteger(systemUiResources.getIdentifier(		
+                        "com.android.systemui:integer/heads_up_notification_decay", null, null));		
+            final int headsUpTimeOut = Settings.System.getInt(getContentResolver(),		
+                    Settings.System.HEADS_UP_NOTIFCATION_DECAY, defaultTimeOut);		
+            mHeadsUpTimeOut.setValue(String.valueOf(headsUpTimeOut));		
+            updateHeadsUpTimeOutSummary(headsUpTimeOut);		
+
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        refreshNotificationListeners();
+        mSettingsObserver.register(true);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSettingsObserver.register(false);
+    }
+
+    private void updateHeadsUpSnoozeTimeSummary(int value) {
+        String summary = value != 0
+                ? getResources().getString(R.string.heads_up_snooze_summary, value / 60 / 1000)
+                : getResources().getString(R.string.heads_up_snooze_disabled_summary);
+        mHeadsUpSnoozeTime.setSummary(summary);
+    }
+
+    private void updateHeadsUpTimeOutSummary(int value) {
+        String summary = getResources().getString(R.string.heads_up_time_out_summary,
+                value / 1000);
+        if (value == 0) {
+            mHeadsUpTimeOut.setSummary(
+                    getResources().getString(R.string.heads_up_time_out_never_summary));
+        } else {
+            mHeadsUpTimeOut.setSummary(summary);
+        }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
 
@@ -49,4 +181,161 @@ public class LiquidnotificationSettings extends SettingsPreferenceFragment {
         boolean value;
  		return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
+
+    // === Pulse notification light === 		
+			
+    private void initPulse(PreferenceCategory parent) {		
+        if (!getResources().getBoolean(		
+                com.android.internal.R.bool.config_intrusiveNotificationLed)) {		
+            parent.removePreference(parent.findPreference(KEY_NOTIFICATION_LIGHT));		
+        }		
+        if (!getResources().getBoolean(		
+                com.android.internal.R.bool.config_intrusiveBatteryLed)		
+                || UserHandle.myUserId() != UserHandle.USER_OWNER) {		
+            parent.removePreference(parent.findPreference(KEY_BATTERY_LIGHT));		
+        }		
+    }		
+	
+    private void updatePulse() {		
+        if (mNotificationPulse == null) {		
+            return;		
+        }		
+        try {		
+            mNotificationPulse.setChecked(Settings.System.getInt(getContentResolver(),		
+                    Settings.System.NOTIFICATION_LIGHT_PULSE) == 1);		
+        } catch (Settings.SettingNotFoundException snfe) {		
+            Log.e(TAG, Settings.System.NOTIFICATION_LIGHT_PULSE + " not found");		
+        }		
+    }
+
+    // === Lockscreen (public / private) notifications ===
+
+    private void initLockscreenNotifications(PreferenceCategory parent) {
+        mLockscreen = (DropDownPreference) parent.findPreference(KEY_LOCK_SCREEN_NOTIFICATIONS);
+        if (mLockscreen == null) {
+            Log.i(TAG, "Preference not found: " + KEY_LOCK_SCREEN_NOTIFICATIONS);
+            return;
+        }
+ 
+        mLockscreen.addItem(R.string.lock_screen_notifications_summary_show,
+                R.string.lock_screen_notifications_summary_show);
+        if (mSecure) {
+            mLockscreen.addItem(R.string.lock_screen_notifications_summary_hide,
+                    R.string.lock_screen_notifications_summary_hide);
+        }
+        mLockscreen.addItem(R.string.lock_screen_notifications_summary_disable,
+                R.string.lock_screen_notifications_summary_disable);
+        updateLockscreenNotifications();
+        mLockscreen.setCallback(new DropDownPreference.Callback() {
+            @Override
+            public boolean onItemSelected(int pos, Object value) {
+                final int val = (Integer) value;
+                if (val == mLockscreenSelectedValue) {
+                    return true;
+                }
+                final boolean enabled = val != R.string.lock_screen_notifications_summary_disable;
+                final boolean show = val == R.string.lock_screen_notifications_summary_show;
+                Settings.Secure.putInt(getContentResolver(),
+                        Settings.Secure.LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS, show ? 1 : 0);
+                Settings.Secure.putInt(getContentResolver(),
+                        Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS, enabled ? 1 : 0);
+                mLockscreenSelectedValue = val;
+                return true;
+            }
+        });
+    }
+
+    private void updateLockscreenNotifications() {
+        if (mLockscreen == null) {
+            return;
+        }
+        final boolean enabled = getLockscreenNotificationsEnabled();
+        final boolean allowPrivate = !mSecure || getLockscreenAllowPrivateNotifications();
+        mLockscreenSelectedValue = !enabled ? R.string.lock_screen_notifications_summary_disable :
+                allowPrivate ? R.string.lock_screen_notifications_summary_show :
+                R.string.lock_screen_notifications_summary_hide;
+        mLockscreen.setSelectedValue(mLockscreenSelectedValue);
+    }
+
+    private boolean getLockscreenNotificationsEnabled() {
+        return Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS, 0) != 0;
+    }
+
+    private boolean getLockscreenAllowPrivateNotifications() {
+        return Settings.Secure.getInt(getContentResolver(),
+                Settings.Secure.LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS, 0) != 0;
+    }
+
+    // === Notification listeners ===
+
+    private void refreshNotificationListeners() {
+        if (mNotificationAccess != null) {
+            final int total = NotificationAccessSettings.getListenersCount(mPM);
+            if (total == 0) {
+                getPreferenceScreen().removePreference(mNotificationAccess);
+            } else {
+                final int n = NotificationAccessSettings.getEnabledListenersCount(mContext);
+                if (n == 0) {
+                    mNotificationAccess.setSummary(getResources().getString(
+                            R.string.manage_notification_access_summary_zero));
+                } else {
+                    mNotificationAccess.setSummary(String.format(getResources().getQuantityString(
+                            R.plurals.manage_notification_access_summary_nonzero,
+                            n, n)));
+                }
+            }
+        }
+    }
+
+    // === Callbacks ===
+
+    private final class SettingsObserver extends ContentObserver {
+        private final Uri NOTIFICATION_LIGHT_PULSE_URI =		
+                Settings.System.getUriFor(Settings.System.NOTIFICATION_LIGHT_PULSE);
+        private final Uri LOCK_SCREEN_PRIVATE_URI =
+                Settings.Secure.getUriFor(Settings.Secure.LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS);
+        private final Uri LOCK_SCREEN_SHOW_URI =
+                Settings.Secure.getUriFor(Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS);
+
+        public SettingsObserver() {
+            super(mHandler);
+        }
+
+        public void register(boolean register) {
+            final ContentResolver cr = getContentResolver();
+            if (register) {
+                cr.registerContentObserver(NOTIFICATION_LIGHT_PULSE_URI, false, this);
+                cr.registerContentObserver(LOCK_SCREEN_PRIVATE_URI, false, this);
+                cr.registerContentObserver(LOCK_SCREEN_SHOW_URI, false, this);
+            } else {
+                cr.unregisterContentObserver(this);
+            }
+        }
+
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            super.onChange(selfChange, uri);
+            if (NOTIFICATION_LIGHT_PULSE_URI.equals(uri)) {		
+                updatePulse();		
+            }
+            if (LOCK_SCREEN_PRIVATE_URI.equals(uri) || LOCK_SCREEN_SHOW_URI.equals(uri)) {
+                updateLockscreenNotifications();
+            }
+        }
+    }
+
+    // === Indexing ===
+
+    public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+
+        public List<SearchIndexableResource> getXmlResourcesToIndex(
+                Context context, boolean enabled) {
+            final SearchIndexableResource sir = new SearchIndexableResource(context);
+            sir.xmlResId = R.xml.notification_settings;
+            return Arrays.asList(sir);
+        }
+    };
+}
 }
