c7200f24c7918ee8de8177509344c586152d246e
snak3ater <saeedyasir91@gmail.com>
2015-01-17 21:55:16 +0000

    QS: Squashed from EuphoriaOS (2/2)
    Change-Id: If9378713650e7ad74d647c36f788771559ed596c

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -75,6 +75,8 @@ import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
+import com.android.settings.slim.DisplayRotation;
+import com.android.settings.slim.qs.QSTiles;
 import com.android.settings.dashboard.DashboardCategory;
 import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.dashboard.DashboardTile;
@@ -313,6 +315,8 @@ public class SettingsActivity extends Activity
             MainSettings.class.getName(),
             ExpandedDesktopPreferenceFragment.class.getName(),
             BlacklistSettings.class.getName()
+            QSTiles.class.getName(),
+            DisplayRotation.class.getName()
     };
 
 
