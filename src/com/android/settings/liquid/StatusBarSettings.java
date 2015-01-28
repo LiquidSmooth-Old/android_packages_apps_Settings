978d7f05c9df3cafc29ba09f3dbba3cb6265c5c9
Jean-Pierre Rasquin <yank555.lu@gmail.com>
2015-01-28 23:53:24 +0100

    [1/2] Settings: Add options for battery icon and text in status bar
    Available options :
    
    - icon : portrait, landscape battery / normal, dotted circle / text only / none
    - battery percent : none / inside icon / next to icon
    
    Charging with portrait and landscape icons :
    
    - without battery percent inside --> show a bolt inside of icon blinking
    - with battery percent inside --> alternate every second between battery percent and bolt inside of icon
    
    Charging with circle icon :
    
    - without battery percent inside --> charged subset of circle will rotate with bolt inside blinking
    - with battery percent inside --> charged subset of circle will rotate with bolt and battery percent alternating inside
    
    Charging text only :
    
    - so far no difference between charging and not charging
    
    Based on Zavon Joseph patchset 2
    
    Extended with percentage text option by Jean-Pierre Rasquin <yank555.lu@gmail.com>
    
    - fixed text options not disabled when entering settings and style is hidden or text
    
    - fixed code layout
    
    - added dottect circle style option (thx to Griffin Millender)
    
    - rearranged icon styles more logically (portrait, landscape, circle, dotted circle, text, hidden)
    
    - code layout review
    
    - dead space removed
    
    - tab removed
    
    Change-Id: Ia6494ec4ed9973b685356407a9e1bf7e5178287d
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>

--- src/com/android/settings/liquid/StatusBarSettings.java
+++ src/com/android/settings/liquid/StatusBarSettings.java
@@ -19,7 +19,10 @@ package com.android.settings.liquid;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.SwitchPreference;
@@ -42,16 +45,27 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     private static final String STATUS_BAR_BRIGHTNESS_CONTROL = "status_bar_brightness_control";
     private static final String KEY_STATUS_BAR_CLOCK = "clock_style_pref";
     private static final String KEY_STATUS_BAR_TICKER = "status_bar_ticker_enabled";
+    private static final String STATUS_BAR_SHOW_BATTERY_PERCENT = "status_bar_show_battery_percent";
+    private static final String STATUS_BAR_BATTERY_STYLE = "status_bar_battery_style";
+    private static final String STATUS_BAR_BATTERY_STYLE_HIDDEN = "4";
+    private static final String STATUS_BAR_BATTERY_STYLE_TEXT = "6";
 
     private SwitchPreference mStatusBarBrightnessControl;
     private PreferenceScreen mClockStyle;
     private SwitchPreference mTicker;
+    private ListPreference mStatusBarBattery;
+    private ListPreference mStatusBarBatteryShowPercent;
+
+    private int mbatteryStyle;
+    private int mbatteryShowPercent;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.liquid_statusbar_settings);
+
+        ContentResolver resolver = getActivity().getContentResolver();
 
         PreferenceScreen prefSet = getPreferenceScreen();
 
@@ -81,10 +95,44 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         mClockStyle = (PreferenceScreen) prefSet.findPreference(KEY_STATUS_BAR_CLOCK);
         updateClockStyleDescription();
 
+        mStatusBarBattery = (ListPreference) findPreference(STATUS_BAR_BATTERY_STYLE);
+        mStatusBarBatteryShowPercent =
+                (ListPreference) findPreference(STATUS_BAR_SHOW_BATTERY_PERCENT);
+
+        mbatteryStyle = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_BATTERY_STYLE, 0);
+        mStatusBarBattery.setValue(String.valueOf(mbatteryStyle));
+        mStatusBarBattery.setSummary(mStatusBarBattery.getEntry());
+        mStatusBarBattery.setOnPreferenceChangeListener(this);
+
+        mbatteryShowPercent = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0);
+        mStatusBarBatteryShowPercent.setValue(String.valueOf(mbatteryShowPercent));
+        mStatusBarBatteryShowPercent.setSummary(mStatusBarBatteryShowPercent.getEntry());
+        mStatusBarBatteryShowPercent.setOnPreferenceChangeListener(this);
+        enableStatusBarBatteryDependents(String.valueOf(mbatteryStyle));
+
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mStatusBarBrightnessControl) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mStatusBarBattery) {
+            mbatteryStyle = Integer.valueOf((String) newValue);
+            int index = mStatusBarBattery.findIndexOfValue((String) newValue);
+            Settings.System.putInt(
+                    resolver, Settings.System.STATUS_BAR_BATTERY_STYLE, mbatteryStyle);
+            mStatusBarBattery.setSummary(mStatusBarBattery.getEntries()[index]);
+            enableStatusBarBatteryDependents((String) newValue);
+            return true;
+        } else if (preference == mStatusBarBatteryShowPercent) {
+            mbatteryShowPercent = Integer.valueOf((String) newValue);
+            int index = mStatusBarBatteryShowPercent.findIndexOfValue((String) newValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, mbatteryShowPercent);
+            mStatusBarBatteryShowPercent.setSummary(
+                    mStatusBarBatteryShowPercent.getEntries()[index]);
+            return true;
+        } else if (preference == mStatusBarBrightnessControl) {
             Settings.System.putInt(getContentResolver(),
                     Settings.System.STATUS_BAR_BRIGHTNESS_CONTROL,
                     (Boolean) newValue ? 1 : 0);
@@ -102,6 +150,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     public void onResume() {
         super.onResume();
         updateClockStyleDescription();
+        enableStatusBarBatteryDependents(String.valueOf(mbatteryStyle));
     }
 
     private void updateClockStyleDescription() {
@@ -114,6 +163,12 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         } else {
             mClockStyle.setSummary(getString(R.string.disabled));
          }
+    }
+
+    private void enableStatusBarBatteryDependents(String value) {
+        boolean enabled = !(value.equals(STATUS_BAR_BATTERY_STYLE_TEXT)
+                || value.equals(STATUS_BAR_BATTERY_STYLE_HIDDEN));
+        mStatusBarBatteryShowPercent.setEnabled(enabled);
     }
 
 }
