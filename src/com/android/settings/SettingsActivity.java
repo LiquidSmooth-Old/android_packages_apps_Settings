e1d1c8b073974ed4002f05b13ea43bc21509a4af
Yashar Ahmedizadeh <yashade2001@gmail.com>
2015-01-29 22:38:31 +0200

    Add setting to launch BitSyko Layers App from Settings
    Credits to: @ProjectDISCO
    Signed-off-by: Yashar Ahmedizadeh <yashade2001@gmail.com>
    
    Also includes commit:
    Switch to vector drawable for layers icon
    ecdb859e83e30471d3ddf6de9fd3b1a1336899b3
    
    Change-Id: I4bfcb297ca990b167c83156d7eebbd3e3117db91

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1209,6 +1209,16 @@ public class SettingsActivity extends Activity
                             UserManager.DISALLOW_DEBUGGING_FEATURES)) {
                         removeTile = true;
                     }
+                } else if (id == R.id.bitsyko_layers) {
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("org.bitsyko.overlaymanager", 0).versionCode > 0);
+                    } catch (PackageManager.NameNotFoundException e) {
+
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                    }
                 }
 
                 if (UserHandle.MU_ENABLED && UserHandle.myUserId() != 0
@@ -1369,3 +1379,4 @@ public class SettingsActivity extends Activity
         mResultIntentData = resultIntentData;
     }
 }
+
