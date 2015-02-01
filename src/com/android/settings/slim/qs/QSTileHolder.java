2fdb73391075a13f12a3843e9e13769d5b79ef1a
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-01 07:23:50 +0000

    QS: add Screenshot & Sync tile (2/2)
    Change-Id: Ieb3eddd45be2a8c6c4bf19b5931c40998794324d

--- src/com/android/settings/slim/qs/QSTileHolder.java
+++ src/com/android/settings/slim/qs/QSTileHolder.java
@@ -137,6 +137,14 @@ public class QSTileHolder {
                 resourceName = "ic_qs_media_play";
                 stringId = R.string.qs_music_play_tile;
                 break;
+            case QSConstants.TILE_SCREENSHOT:
+                resourceName = "ic_qs_screenshot";
+                stringId = R.string.qs_screenshot_tile;
+                break;
+            case QSConstants.TILE_SYNC:
+                resourceName = "ic_qs_sync_on";
+                stringId = R.string.qs_sync_tile;
+                break;
             default:
                 return null;
         }
