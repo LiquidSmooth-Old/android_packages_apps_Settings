5f6f8dcfd854af36f494aa6befb660e8db49725b
deadman96385 <seanhoyt963@gmail.com>
2015-02-25 21:51:05 -0500

    Implement Kernel Auditor (Tweaker) (1/2)
    Change-Id: I9486b963ebe575830db9ae551f4c213c801f2c6e

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1243,6 +1243,16 @@ public class SettingsActivity extends Activity
                     if (!supported) {
                         removeTile = true;
                     }
+                } else if (id == R.id.kernel_auditor) {
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("com.grarak.kerneladiutor", 0).versionCode > 0);
+                    } catch (PackageManager.NameNotFoundException e) {
+
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                    }
                 }
 
                 if (UserHandle.MU_ENABLED && UserHandle.myUserId() != 0
