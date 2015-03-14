SlimRoms/packages_apps_Settings

Privacy Guard Slim version (2/2)
Re-implements slims ehanced privacy guard from 4.3
and adapt it to 4.4 changes

Change-Id: Ie87a72b4212e5a8063c1dfbf7fd759f3a14a0d12

src/com/android/settings/slim/privacyguard/PrivacyGuardPrefs.java
@@ -1,0 +1,104 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.slim.privacyguard;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class PrivacyGuardPrefs extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "PrivacyGuardPrefs";
+
+    private static final String KEY_PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
+    private static final String KEY_PRIVACY_GUARD_NOTIFICATION = "privacy_guard_notification";
+
+    private CheckBoxPreference mPrivacyGuardDefault;
+    private CheckBoxPreference mPrivacyGuardNotification;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.privacy_guard_prefs);
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mPrivacyGuardDefault = (CheckBoxPreference) findPreference(KEY_PRIVACY_GUARD_DEFAULT);
+        mPrivacyGuardDefault.setOnPreferenceChangeListener(this);
+
+        try {
+            mPrivacyGuardDefault.setChecked(Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_DEFAULT) == 1);
+        } catch (SettingNotFoundException e) {
+            mPrivacyGuardDefault.setChecked(false);
+        }
+
+        mPrivacyGuardNotification =
+            (CheckBoxPreference) findPreference(KEY_PRIVACY_GUARD_NOTIFICATION);
+        mPrivacyGuardNotification.setOnPreferenceChangeListener(this);
+
+        try {
+            mPrivacyGuardNotification.setChecked(Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_NOTIFICATION) == 1);
+        } catch (SettingNotFoundException e) {
+            mPrivacyGuardNotification.setChecked(true);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        int paddingTop = list.getPaddingTop();
+        int paddingBottom = list.getPaddingBottom();
+        list.setPadding(0, paddingTop, 0, paddingBottom);
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mPrivacyGuardDefault) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_DEFAULT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPrivacyGuardNotification) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_NOTIFICATION, value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+}

LiquidSmooth-Old/android_packages_apps_Settings

Conflicts:
	src/com/android/settings/SecuritySettings.java

src/com/android/settings/slim/privacyguard/PrivacyGuardPrefs.java
@@ -1,0 +1,104 @@
+/*
+ * Copyright (C) 2013 Slimroms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.slim.privacyguard;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.provider.Settings.SettingNotFoundException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class PrivacyGuardPrefs extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String TAG = "PrivacyGuardPrefs";
+
+    private static final String KEY_PRIVACY_GUARD_DEFAULT = "privacy_guard_default";
+    private static final String KEY_PRIVACY_GUARD_NOTIFICATION = "privacy_guard_notification";
+
+    private SwitchPreference mPrivacyGuardDefault;
+    private SwitchPreference mPrivacyGuardNotification;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.privacy_guard_prefs);
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mPrivacyGuardDefault = (SwitchPreference) findPreference(KEY_PRIVACY_GUARD_DEFAULT);
+        mPrivacyGuardDefault.setOnPreferenceChangeListener(this);
+
+        try {
+            mPrivacyGuardDefault.setChecked(Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_DEFAULT) == 1);
+        } catch (SettingNotFoundException e) {
+            mPrivacyGuardDefault.setChecked(false);
+        }
+
+        mPrivacyGuardNotification =
+            (SwitchPreference) findPreference(KEY_PRIVACY_GUARD_NOTIFICATION);
+        mPrivacyGuardNotification.setOnPreferenceChangeListener(this);
+
+        try {
+            mPrivacyGuardNotification.setChecked(Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_NOTIFICATION) == 1);
+        } catch (SettingNotFoundException e) {
+            mPrivacyGuardNotification.setChecked(true);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+            ViewGroup container, Bundle savedInstanceState) {
+        final View view = super.onCreateView(inflater, container, savedInstanceState);
+        final ListView list = (ListView) view.findViewById(android.R.id.list);
+        // our container already takes care of the padding
+        int paddingTop = list.getPaddingTop();
+        int paddingBottom = list.getPaddingBottom();
+        list.setPadding(0, paddingTop, 0, paddingBottom);
+        return view;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mPrivacyGuardDefault) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_DEFAULT, value ? 1 : 0);
+            return true;
+        } else if (preference == mPrivacyGuardNotification) {
+            boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(getContentResolver(),
+                    Settings.Secure.PRIVACY_GUARD_NOTIFICATION, value ? 1 : 0);
+            return true;
+        }
+        return false;
+    }
+}