9ac381e40538bd4f12ec2ed4fe9ba70abcf206a9
Miccia <bono.michele94@gmail.com>
2015-03-20 16:08:12 +0100

    HFM: Add whitelist to HFM
    * also fix not being able to switch off the ad disabler
    
    AICPfy
    CheckBox to Switch
    Move settings to main Extras menu
    
    Change-Id: Ifa8adf2313f0c0932882c3ac6cd4bda7e8b250de
    
    Conflicts:
    	AndroidManifest.xml

--- src/com/android/settings/liquid/hfm/HfmSettings.java
+++ src/com/android/settings/liquid/hfm/HfmSettings.java
@@ -20,9 +20,11 @@ import java.io.IOException;
 import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.preference.SwitchPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceScreen;
@@ -33,6 +35,7 @@ import android.view.View;
 
 import com.android.settings.liquid.hfm.FetchHosts;
 import com.android.settings.liquid.hfm.HfmHelpers;
+import com.android.settings.liquid.hfm.HfmWhitelist;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
@@ -40,12 +43,14 @@ public class HfmSettings extends SettingsPreferenceFragment {
 
     private static final String TAG = "HfmSettings";
     private static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+    private static final String KEY_HFM_WHITELIST = "hfm_whitelist";
 
     public static ProgressDialog pd;
 
     ConnectivityManager connMgr;
 
     public static SwitchPreference mHfmDisableAds;
+    private Preference mHfmWhitelist;
     Preference mHfmUpdateHosts;
 
     Context context;
@@ -71,6 +76,14 @@ public class HfmSettings extends SettingsPreferenceFragment {
         mHfmDisableAds.setChecked((Settings.System.getInt(resolver,
             Settings.System.HFM_DISABLE_ADS, 0) == 1));
         mHfmUpdateHosts = prefScreen.findPreference("hfm_update_hosts");
+
+        mHfmWhitelist = (Preference) findPreference(KEY_HFM_WHITELIST);
+
+        if (mHfmDisableAds.isChecked()) {
+            mHfmWhitelist.setEnabled(true);
+        } else {
+            mHfmWhitelist.setEnabled(false);
+        }
     }
 
     @Override
@@ -90,6 +103,11 @@ public class HfmSettings extends SettingsPreferenceFragment {
             boolean checked = ((SwitchPreference)preference).isChecked();
             Settings.System.putInt(getActivity().getContentResolver(),
                 Settings.System.HFM_DISABLE_ADS, checked ? 1:0);
+            if (checked) {
+                mHfmWhitelist.setEnabled(true);
+            } else {
+                mHfmWhitelist.setEnabled(false);
+            }
             HfmHelpers.checkStatus(getActivity());
         } else if (preference == mHfmUpdateHosts) {
             try {
@@ -97,6 +115,8 @@ public class HfmSettings extends SettingsPreferenceFragment {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        } else if  (preference == mHfmWhitelist) {
+             startActivity(new Intent(getActivity(), HfmWhitelist.class));
         } else {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
