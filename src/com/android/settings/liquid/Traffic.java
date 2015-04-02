139d3502721760cb735045576728ce40f9257ec3
Jason H Weitzel <jsnweitzel@gmail.com>
2015-04-02 15:50:36 -0400

    fixes 	modified:   res/xml/device_info_status.xml 	modified:   res/xml/liquid_main_settings.xml 	modified:   res/xml/liquid_navigation_settings.xml 	deleted:    src/com/android/settings/cyanogenmod/SecureSettingSwitchPreference.java 	modified:   src/com/android/settings/liquid/Traffic.java

--- src/com/android/settings/liquid/Traffic.java
+++ src/com/android/settings/liquid/Traffic.java
@@ -36,7 +36,7 @@ import android.view.MenuItem;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.widget.SeekBarPreferenceCham
+import com.android.settings.widget.SeekBarPreferenceCham;
 
 import net.margaritov.preference.colorpicker.ColorPickerPreference;
 
