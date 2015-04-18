f8eea76935b90bf62e132d77c96d481b5a38cac3
Jason H Weitzel <jsnweitzel@gmail.com>
2015-04-17 20:34:03 -0400

    Revert "Settings: some fixes and addaptions to LP for PGM"
    This reverts commit d39b89940c6cf0afbe468bdb53d119ce3f8192ca.
    
    Conflicts:
    	res/drawable-hdpi/ic_settings_reset.png
    	res/drawable-mdpi/ic_settings_reset.png
    	res/drawable-xhdpi/ic_settings_reset.png
    	res/drawable-xxhdpi/ic_settings_reset.png
    	src/com/android/settings/SecuritySettings.java
    
    	modified:   src/com/android/settings/SecuritySettings.java
    	modified:   src/com/android/settings/applications/AppOpsDetails.java
    	modified:   src/com/android/settings/applications/AppOpsState.java
    	modified:   src/com/android/settings/slim/privacyguard/PrivacyGuardManager.java
    
    Change-Id: Ie27a441958b3107cd909ddcdc8b3cca1ce0fd056

--- src/com/android/settings/SecuritySettings.java
+++ src/com/android/settings/SecuritySettings.java
@@ -212,15 +212,12 @@ public class SecuritySettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.security_settings);
         root = getPreferenceScreen();
 
-        // Add options for device encryption
-        mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
-
-        // App security settings
-        addPreferencesFromResource(R.xml.security_settings_app_slim);
-
         // Add options for lock/unlock screen
         final int resid = getResIdForLockUnlockScreen(getActivity(), mLockPatternUtils);
         addPreferencesFromResource(resid);
+
+        // Add options for device encryption
+        mIsPrimary = UserHandle.myUserId() == UserHandle.USER_OWNER;
 
         if (!mIsPrimary) {
             // Rename owner info settings
