46c0c7954e78ae29b556c46589fb17d26ac1454d
Michael Bestas <mikeioannina@gmail.com>
2015-04-22 01:38:22 +0300

    QS: Add heads up tile (2/2)
    Change-Id: I27eedf95ed200ab24f79380d6dd29c676775806b

--- src/com/android/settings/slim/qs/QSTileHolder.java
+++ src/com/android/settings/slim/qs/QSTileHolder.java
@@ -161,6 +161,10 @@ public class QSTileHolder {
                 resourceName = "ic_qs_expanded_desktop";
                 stringId = R.string.qs_expanded_desktop_tile;
                 break;
+            case QSConstants.TILE_HEADS_UP:
+                resourceName = "ic_qs_heads_up_on";
+                stringId = R.string.qs_tile_heads_up;
+                break;
             default:
                 return null;
         }
