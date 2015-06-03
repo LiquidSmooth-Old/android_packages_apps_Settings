8c96dee4e0dd3d106f5142914ad839605fde3ef9
deadman96385 <seanhoyt963@gmail.com>
2015-06-03 15:39:50 -0400

    Revert "Add setting to launch BitSyko Layers App from Settings"
    This reverts commit face6994193da0364750c8b3f3c13cf4ebcbdbb7.
    
    Conflicts:
    	res/values/strings.xml
    	res/xml/dashboard_categories.xml
    	src/com/android/settings/SettingsActivity.java
    
    Change-Id: Iebe744beb30bb8a9fb84ea44321f9401a3792c56

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1242,16 +1242,6 @@ public class SettingsActivity extends Activity
                     if (!supported) {
                         removeTile = true;
                     }
-                } else if (id == R.id.bitsyko_layers) {
-                    boolean supported = false;
-                    try {
-                        supported = (getPackageManager().getPackageInfo("com.lovejoy777.rroandlayersmanager", 0).versionCode > 0);
-                    } catch (PackageManager.NameNotFoundException e) {
-
-                    }
-                    if (!supported) {
-                        removeTile = true;
-                    }
                 } else if (id == R.id.kernel_auditor) {
                     boolean supported = false;
                     try {
