1bf46d1a5c7501509bd2c6903c070c57dccb52bf
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-07-19 00:15:59 +0100

    Ls weather: options to hide the weather when notifications are visible (2/2)
    Hide the weather when:
      - Visible notifications reach the maximum allowed notifications on the lock screen
        (including the notification overlay container)
      - Visible notifications reach a customizable number of notifications
      - Never
    
    Customizable number of notifications (to hide the weather panel):
      - 1 (always)
      - 2
      - 3
      - 4
      - 5
      - 6
    
    Change-Id: I32adbe42c95ecd6c405bcd166079c4d2077b76de

--- src/com/android/settings/euphoria/LockScreenWeatherSettings.java
+++ src/com/android/settings/euphoria/LockScreenWeatherSettings.java
@@ -26,42 +26,77 @@ import android.provider.Settings;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.euphoria.SeekBarPreference;
 
 public class LockScreenWeatherSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String PREF_CONDITION_ICON =
             "weather_condition_icon";
+    private static final String PREF_HIDE_WEATHER =
+            "weather_hide_panel";
+    private static final String PREF_NUMBER_OF_NOTIFICATIONS =
+            "weather_number_of_notifications";
 
     private static final int MONOCHROME_ICON = 0;
 
     private ListPreference mConditionIcon;
+    private ListPreference mHideWeather;
+    private SeekBarPreference mNumberOfNotifications;
 
     private ContentResolver mResolver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        refreshSettings();
-    }
-
-    public void refreshSettings() {
-        PreferenceScreen prefs = getPreferenceScreen();
-        if (prefs != null) {
-            prefs.removeAll();
-        }
-
         addPreferencesFromResource(R.xml.lock_screen_weather_settings);
         mResolver = getActivity().getContentResolver();
-
-        int conditionIcon = Settings.System.getInt(mResolver,
-               Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, MONOCHROME_ICON);
+        PreferenceScreen prefs = getPreferenceScreen();
 
         mConditionIcon =
                 (ListPreference) findPreference(PREF_CONDITION_ICON);
+        int conditionIcon = Settings.System.getInt(mResolver,
+               Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, MONOCHROME_ICON);
         mConditionIcon.setValue(String.valueOf(conditionIcon));
         mConditionIcon.setSummary(mConditionIcon.getEntry());
         mConditionIcon.setOnPreferenceChangeListener(this);
+
+        mHideWeather =
+                (ListPreference) findPreference(PREF_HIDE_WEATHER);
+        int hideWeather = Settings.System.getInt(mResolver,
+               Settings.System.LOCK_SCREEN_WEATHER_HIDE_PANEL, 0);
+        mHideWeather.setValue(String.valueOf(hideWeather));
+        mHideWeather.setOnPreferenceChangeListener(this);
+
+        mNumberOfNotifications =
+                (SeekBarPreference) findPreference(PREF_NUMBER_OF_NOTIFICATIONS);
+        int numberOfNotifications = Settings.System.getInt(mResolver,
+                Settings.System.LOCK_SCREEN_WEATHER_NUMBER_OF_NOTIFICATIONS, 4);
+        mNumberOfNotifications.setValue(numberOfNotifications);
+        mNumberOfNotifications.setOnPreferenceChangeListener(this);
+
+        updatePreference();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updatePreference();
+    }
+
+    private void updatePreference() {
+        int hideWeather = Settings.System.getInt(mResolver,
+                Settings.System.LOCK_SCREEN_WEATHER_HIDE_PANEL, 0);
+        if (hideWeather == 0) {
+            mNumberOfNotifications.setEnabled(false);
+            mHideWeather.setSummary(R.string.weather_hide_panel_auto_summary);
+        } else if (hideWeather == 1) {
+            mNumberOfNotifications.setEnabled(true);
+            mHideWeather.setSummary(R.string.weather_hide_panel_custom_summary);
+        } else {
+            mNumberOfNotifications.setEnabled(false);
+            mHideWeather.setSummary(R.string.weather_hide_panel_never_summary);
+        }
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -72,7 +107,19 @@ public class LockScreenWeatherSettings extends SettingsPreferenceFragment implem
             Settings.System.putInt(mResolver,
                     Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, intValue);
             mConditionIcon.setSummary(mConditionIcon.getEntries()[index]);
-            refreshSettings();
+            return true;
+        } else if (preference == mHideWeather) {
+            int intValue = Integer.valueOf((String) newValue);
+            int index = mHideWeather.findIndexOfValue((String) newValue);
+            Settings.System.putInt(mResolver,
+                    Settings.System.LOCK_SCREEN_WEATHER_HIDE_PANEL, intValue);
+            updatePreference();
+            return true;
+        } else if (preference == mNumberOfNotifications) {
+            int numberOfNotifications = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.LOCK_SCREEN_WEATHER_NUMBER_OF_NOTIFICATIONS,
+            numberOfNotifications);
             return true;
         }
         return false;
