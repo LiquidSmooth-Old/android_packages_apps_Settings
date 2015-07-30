879f3676ab93c093ed7a5630114fb7c3b6a019bf
Roman Birg <roman@cyngn.com>
2015-07-30 15:20:52 -0700

    Settings: fix a few color picker inconsistentcies.
    Don't assume we can pulse when calling setAllValues().
    
    Don't display both on/off pulse values when it cannot pulse.
    
    Default to Always on when we can't pulse.
    
    Ref: CYNGNOS-403
    Change-Id: Ie356cdc35cb545dd51896697f89ef265ada1147f
    Signed-off-by: Roman Birg <roman@cyngn.com>
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- src/com/android/settings/notificationlight/ApplicationLightPreference.java
+++ src/com/android/settings/notificationlight/ApplicationLightPreference.java
@@ -147,7 +147,7 @@ public class ApplicationLightPreference extends DialogPreference {
             mOnValueView.setText(mapLengthValue(mOnValue));
         }
         if (mOffValueView != null) {
-            if (mOnValue == 1) {
+            if (mOnValue == 1 || !mOnOffChangeable) {
                 mOffValueView.setVisibility(View.GONE);
             } else {
                 mOffValueView.setVisibility(View.VISIBLE);
@@ -225,7 +225,6 @@ public class ApplicationLightPreference extends DialogPreference {
         mColorValue = color;
         mOnValue = onValue;
         mOffValue = offValue;
-        mOnOffChangeable = true;
         updatePreferenceViews();
     }
 
@@ -259,6 +258,9 @@ public class ApplicationLightPreference extends DialogPreference {
     }
 
     private String mapLengthValue(Integer time) {
+        if (!mOnOffChangeable) {
+            return getContext().getString(R.string.pulse_length_always_on);
+        }
         if (time == DEFAULT_TIME) {
             return getContext().getString(R.string.default_time);
         }
