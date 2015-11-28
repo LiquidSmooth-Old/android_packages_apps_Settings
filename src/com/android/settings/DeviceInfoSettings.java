b5e2d3a905743f4058df288b35b3aec749e7547f
Jason H Weitzel <jsnweitzel@gmail.com>
2015-11-28 08:44:53 -0500

    add liquid version 	new file:   res/values/liquid_strings.xml 	modified:   res/xml/device_info_settings.xml 	modified:   src/com/android/settings/DeviceInfoSettings.java
    Change-Id: I1e156784ef175e4dd73be4a77bc16329c1de2ff3

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -73,6 +73,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String KEY_KERNEL_VERSION = "kernel_version";
     private static final String KEY_BUILD_NUMBER = "build_number";
     private static final String KEY_DEVICE_MODEL = "device_model";
+    private static final String KEY_LIQUID_VERSION = "liquid_version";
     private static final String KEY_SELINUX_STATUS = "selinux_status";
     private static final String KEY_BASEBAND_VERSION = "baseband_version";
     private static final String KEY_FIRMWARE_VERSION = "firmware_version";
@@ -126,6 +127,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL + getMsvSuffix());
         setValueSummary(KEY_EQUIPMENT_ID, PROPERTY_EQUIPMENT_ID);
         setStringSummary(KEY_DEVICE_MODEL, Build.MODEL);
+        setValueSummary(KEY_LIQUID_VERSION, "ro.liquid.version");
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
